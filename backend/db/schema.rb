c20cb4c53d317aaa187eb7b7108df6fe2b7a5236
Fred Rocher <frocher2005@gmail.com>
2019-08-16 19:43:55 +0200

    Create schema

--- backend/db/schema.rb
+++ backend/db/schema.rb
@@ -12,7 +12,7 @@
 
 ActiveRecord::Schema.define(version: 20181016193336) do
 
-  create_table "budgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "budgets", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "page_id", null: false
     t.integer "category", default: 0, null: false
     t.integer "item", default: 0, null: false
@@ -22,7 +22,7 @@ ActiveRecord::Schema.define(version: 20181016193336) do
     t.index ["page_id"], name: "index_budgets_on_page_id"
   end
 
-  create_table "identities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "identities", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "provider", null: false
     t.string "uid", null: false
     t.integer "user_id", null: false
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20181016193336) do
     t.index ["provider", "uid"], name: "index_identities_on_provider_and_uid", unique: true
   end
 
-  create_table "page_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "page_members", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.integer "user_id", null: false
     t.integer "page_id", null: false
     t.integer "role", default: 0, null: false
@@ -42,14 +42,14 @@ ActiveRecord::Schema.define(version: 20181016193336) do
     t.index ["user_id"], name: "index_page_members_on_user_id"
   end
 
-  create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "pages", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "name"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "screenshot_file_name"
     t.string "screenshot_content_type"
-    t.integer "screenshot_file_size"
+    t.bigint "screenshot_file_size"
     t.datetime "screenshot_updated_at"
     t.string "uptime_keyword"
     t.string "uptime_keyword_type"
@@ -57,12 +57,12 @@ ActiveRecord::Schema.define(version: 20181016193336) do
     t.string "slack_channel"
     t.boolean "mail_notify", default: true
     t.boolean "slack_notify", default: false
-    t.integer "uptime_status", default: 0
+    t.integer "uptime_status", default: 1
     t.boolean "push_notify", default: true
     t.integer "device", default: 0
   end
 
-  create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "subscriptions", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "endpoint", null: false
     t.string "p256dh", null: false
     t.string "auth", null: false
@@ -71,7 +71,7 @@ ActiveRecord::Schema.define(version: 20181016193336) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
+  create_table "users", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=latin1" do |t|
     t.string "provider", null: false
     t.string "uid", default: "", null: false
     t.boolean "admin", default: false
