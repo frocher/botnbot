96d7c24762f8efab479a0ac20d85193d1a11b8f9
Fred Rocher <frocher2005@gmail.com>
2020-04-19 15:08:31 +0200

    Fix environment variable keys

--- backend/app/controllers/environment_controller.rb
+++ backend/app/controllers/environment_controller.rb
@@ -1,9 +1,9 @@
 class EnvironmentController < ApplicationController
   def index
     resu = Hash.new
-    resu["GOOGLE_ANALYTICS_KEY"] = Figaro.env.google_analytics_key
-    resu["PUSH_KEY"] = Figaro.env.push_public_key
-    resu["STRIPE_KEY"] = Figaro.env.stripe_public_key
+    resu["analyticsKey"] = Figaro.env.google_analytics_key
+    resu["pushKey"] = Figaro.env.push_public_key
+    resu["stripeKey"] = Figaro.env.stripe_public_key
     render json: resu
   end
 end
