421740672cf93822f8e5c427929bca4190b48291
Fred Rocher <frocher2005@gmail.com>
2020-05-01 21:26:19 +0200

    Fix only use thunk middleware

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,7 +1,9 @@
 
 import { configureStore } from '@reduxjs/toolkit'
 import rootReducer from './reducers/rootReducer';
+import thunk from 'redux-thunk';
 
 export const store = configureStore({
   reducer: rootReducer,
+  middleware: [thunk],
 });
